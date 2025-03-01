d30afcd375f8a360607702975605ae0e54235bf6
lukaofirst <lucasmacedo123@gmail.com>
2025-03-01 18:31:30 -0300

    using primary constructor

--- Application/Services/PersonEventService.cs
+++ Application/Services/PersonEventService.cs
@@ -7,12 +7,10 @@ namespace Application.Services;
 
 public class PersonEventService(IKafkaProducer kafkaProducer) : IPersonEventService
 {
-	private readonly IKafkaProducer _kafkaProducer = kafkaProducer;
-
 	public async Task ProduceEvent(string topicName, Person person)
 	{
 		var messageBody = JsonSerializer.Serialize(person);
 
-		await _kafkaProducer.ProduceEvent(topicName, messageBody);
+		await kafkaProducer.ProduceEvent(topicName, messageBody);
 	}
 }
